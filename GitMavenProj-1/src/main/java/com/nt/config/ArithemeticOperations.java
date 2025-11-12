562150ac58b09792bd67f7a8d314e3278d321edc
kalyanramnadhagiri <kr2235520@gmail.com>
2025-11-12 11:28:26 +0530

    Kalyan Added ArithemeticOperations.java

---
+++ GitMavenProj-1/src/main/java/com/nt/config/ArithemeticOperations.java
@@ -0,0 +1,22 @@
+package com.nt.config;
+
+public class ArithemeticOperations 
+{
+	public long add(long x,long y)
+	{
+		return x+y;
+	}
+	public long sub(long x,long y)
+	{
+		return x-y;
+	}
+	public long mul(long x,long y)
+	{
+		return x*y;
+	}
+	public long div(long a,long b)
+	{
+		return a/b;
+	}
+
+}
---
+++ GitMavenProj-1/src/main/java/com/nt/main/TestClass.java
@@ -0,0 +1,16 @@
+package com.nt.main;
+
+import com.nt.config.ArithemeticOperations;
+
+public class TestClass 
+{
+	public static void main(String[] args)
+	{
+		ArithemeticOperations op=new ArithemeticOperations();
+		System.out.println(op.add(12222l, 1111119l));
+		System.out.println(op.sub(9999999l, 89000009l));
+		System.out.println(op.mul(235, 5));
+		System.out.println(op.div(1111111, 1000000));
+	}
+
+}
