eb02a38c890b0f06149bc37c6a8e250055517d24
sathya <sathy@sathya.mshome.net>
2025-11-12 15:33:02 +0530

    updated netbanking

--- GitMavenProj-1/src/main/java/com/nt/cfgs/NetBanking.java
+++ GitMavenProj-1/src/main/java/com/nt/cfgs/NetBanking.java
@@ -2,8 +2,17 @@ package com.nt.cfgs;
 
 public class NetBanking
 {
+	long amt;
+	
 	public String addBal(long acNo,long amt)
 	{
 		return amt+"/rs- Successfully Added into the "+acNo;
+	}
+	public int remBal()
+	{
+		
+		 int am=(int) (amt-200);
+		
+		return am;
 	}
 }
