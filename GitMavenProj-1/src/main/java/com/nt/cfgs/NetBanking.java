1567b8a3c6142afbc40ea7076b0cec95bbcee4ad
kalyanramnadhagiri <kr2235520@gmail.com>
2025-11-12 11:40:06 +0530

    Added NetBanking

---
+++ GitMavenProj-1/src/main/java/com/nt/cfgs/NetBanking.java
@@ -0,0 +1,9 @@
+package com.nt.cfgs;
+
+public class NetBanking
+{
+	public String addBal(long acNo,long amt)
+	{
+		return amt+"/rs- Successfully Added into the "+acNo;
+	}
+}
--- GitMavenProj-1/src/main/java/com/nt/main/TestClass.java
+++ GitMavenProj-1/src/main/java/com/nt/main/TestClass.java
@@ -1,5 +1,6 @@
 package com.nt.main;
 
+import com.nt.cfgs.NetBanking;
 import com.nt.config.ArithemeticOperations;
 
 public class TestClass 
@@ -11,6 +12,8 @@ public class TestClass
 		System.out.println(op.sub(9999999l, 89000009l));
 		System.out.println(op.mul(235, 5));
 		System.out.println(op.div(1111111, 1000000));
+		NetBanking nb=new NetBanking();
+		System.out.println(nb.addBal(12345678, 20000));
 	}
 
 }
